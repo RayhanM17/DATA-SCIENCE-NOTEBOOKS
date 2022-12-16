0b1eead92862537d631a38c68859992f267489a0
RayhanM17 <raysdigitalservice@gmail.com>
2022-12-16 00:21:56 -0500

    calcbumpv1

--- JN/Airline-booking.py
+++ JN/Airline-booking.py
@@ -6,18 +6,16 @@ import matplotlib.pyplot as plt
 #Important Variables
 no_show_mu = 0.02434
 no_show_sigma = 0.02436
-plane_size = 30
+plane_capacity = 30
 
 #Generate no show rates
 def gen_no_show_rates(number):
     x = np.random.normal(no_show_mu, no_show_sigma, number)
     x = x[x > 0]
-    plt.hist(x, bins=50)
-    plt.title(str(x.size) + ' no-show rates')
+    #plt.hist(x, bins=50)
+    #plt.title(str(x.size) + ' no-show rates')
     #plt.show()
     return x
-
-no_show_rates = gen_no_show_rates(100000)
 
 #Simulate attendance
 def sim_shows(probs, passengers):
@@ -28,6 +26,16 @@ def sim_shows(probs, passengers):
                                       size=1000) # number of trials
         sim_data[i] = x
     return sim_data
+
+# calculate bump rate, capacity percentage - WORK IN PROGRESS
+def calc_bump(attendance):
+    bump_rate = attendance[attendance > plane_capacity].size / (attendance.size * 1000)
+    print(bump_rate)
+
+# no_show_rates = gen_no_show_rates(100000)
+# plane_attendance = sim_shows(no_show_rates, 34)
+# calc_bump(plane_attendance)
+
 
 
 
