9e206a3b58f2bdb2835671a33be357b39e606a00
RayhanM17 <raysdigitalservice@gmail.com>
2022-12-15 23:46:30 -0500

    show simulation

--- JN/Airline-booking.py
+++ JN/Airline-booking.py
@@ -1,21 +1,36 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import scipy.stats as stats
+#import scipy.stats as stats
 
-
+#Important Variables
 no_show_mu = 0.02434
 no_show_sigma = 0.02436
+plane_size = 30
 
-def calc_no_show_rates(number):
+#Generate no show rates
+def gen_no_show_rates(number):
     x = np.random.normal(no_show_mu, no_show_sigma, number)
     x = x[x > 0]
     plt.hist(x, bins=50)
     plt.title(str(x.size) + ' no-show rates')
-    plt.show()
-    return x.size
+    #plt.show()
+    return x
 
-planes = calc_no_show_rates(100000)
+no_show_rates = gen_no_show_rates(100000)
+
+#Simulate attendance
+def sim_shows(probs, passengers):
+    sim_data = np.zeros((probs.size, 1000)) #2d array of zeros
+    for i in range(0, probs.size):
+        x = np.random.binomial(n=passengers,  # Number of passengers per trial
+                                      p=1 - probs[i],  # show probability
+                                      size=1000) # number of trials
+        sim_data[i] = x
+    return sim_data
+
+
+
 
 
 
