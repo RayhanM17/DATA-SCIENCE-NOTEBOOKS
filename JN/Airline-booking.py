25975e0037e0c5187f6cf1dde5a572ad11f8750a
RayhanM17 <raysdigitalservice@gmail.com>
2022-12-17 19:38:24 -0500

    master_simv1

--- JN/Airline-booking.py
+++ JN/Airline-booking.py
@@ -14,7 +14,6 @@ max_compensation = 1550
 def gen_no_show_rates(number):
     x = np.random.normal(no_show_mu, no_show_sigma, number)
     x[x < 0] = 0
-    print(x[x < 0])
     #plt.hist(x, bins=50)
     #plt.title(str(number) + ' no-show rates')
     #plt.show()
@@ -42,23 +41,17 @@ def calc_profit(attendance, bumps):
     x = bumps * max_compensation
     y = (abs(attendance[attendance < plane_capacity] - plane_capacity)).sum() * ticket_price
     attendance[attendance > plane_capacity] = plane_capacity
-    z = attendance.sum() / (attendance.size * plane_capacity)
+    z = (attendance.sum() / (attendance.size * plane_capacity)).round(4)
     return x, y, z
 
-
-
+# MASTER SIM - WORK IN PROGRESS
+def overbooking_sim():
+    sim_data = pd.Dataframe(
+        {},
+    )
 
 
 no_show_rates = gen_no_show_rates(10000)
 plane_attendance = sim_shows(no_show_rates, 220, 1000)
 overbooked_flights, total_bumps, bump_rate = calc_bump(plane_attendance, 220)
-calc_profit(plane_attendance, total_bumps)
-
-
-
-
-
-
-
-
-
+print(calc_profit(plane_attendance, total_bumps))
