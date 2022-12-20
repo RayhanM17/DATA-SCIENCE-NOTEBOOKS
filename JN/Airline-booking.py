0fc8a23a128a5648ad487923cafc0d1b60116b41
RayhanM17 <raysdigitalservice@gmail.com>
2022-12-19 19:35:38 -0500

    paperv1

--- JN/Airline-booking.py
+++ JN/Airline-booking.py
@@ -11,22 +11,31 @@ ticket_price = 1016.94
 max_compensation = 1550
 
 #Generate no show rates
-def gen_no_show_rates(number):
+def gen_no_show_rates(number, graph):
     x = np.random.normal(no_show_mu, no_show_sigma, number)
     x[x < 0] = 0
-    #plt.hist(x, bins=50)
-    #plt.title(str(number) + ' no-show rates')
-    #plt.show()
+
+    if graph:
+        plt.hist(x, bins=50)
+        plt.title(str(number) + ' no-show rates')
+        plt.show()
     return x
 
 #Simulate attendance
-def sim_shows(probs, booked, trials):
+def sim_shows(probs, booked, trials, graph):
     sim_data = np.zeros((probs.size, trials)) #2d array of zeros
     for i in range(0, probs.size):
         x = np.random.binomial(n=booked,  # Number of passengers booked per trial
                                       p=1 - probs[i],  # show probability
                                       size=trials) # number of trials
         sim_data[i] = x
+    if graph:
+        pa = sim_data.flatten()
+        r = int((max(pa) - min(pa)))
+        plt.xlabel("Number of passengers arrived")
+        plt.title("Attendance when " + str(booked) + ' are booked')
+        plt.hist(pa, r)
+        plt.show()
     return sim_data
 
 # number of overflow flights, total bumps, & bump rate
@@ -36,17 +45,19 @@ def calc_bump(attendance, booked):
     z = y / (attendance.size * booked)
     return x, y, z
 
-# calculate compensation, empty seats, opportunity lost, capacity
+# calculate compensation, empty seats, opportunity lost, capacity, profit
 def calc_profit(attendance, bumps):
-    w = bumps * max_compensation
-    x = (abs(attendance[attendance < plane_capacity] - plane_capacity)).sum()
-    y = x * ticket_price
+    v = bumps * max_compensation
+    w = (abs(attendance[attendance < plane_capacity] - plane_capacity)).sum()
+    x = w * ticket_price
     attendance[attendance > plane_capacity] = plane_capacity
-    z = (attendance.sum() / (attendance.size * plane_capacity)).round(4)
-    return w, x, y, z
+    y = (attendance.sum() / (attendance.size * plane_capacity)).round(4)
+    z = attendance.sum() * ticket_price - bumps * max_compensation
 
-# MASTER SIM - WORK IN PROGRESS
-def overbooking_sim(data_points, runs):
+    return v, w, x, y, z
+
+# MASTER SIM - params = number of over booking, runs, if you want to graph
+def overbooking_sim(data_points, runs, graph):
     sim_data = pd.DataFrame(
         {},
         columns=np.arange(
@@ -60,14 +71,15 @@ def overbooking_sim(data_points, runs):
             'total compensation',
             'empty seats',
             'opportunity lost',
-            'plane capacity']
+            'plane capacity',
+            'profit']
     )
-    no_show_rates = gen_no_show_rates(int(runs * 0.001))
+    no_show_rates = gen_no_show_rates(int(runs * 0.001), False)
     for i in range(1, 21):
         # calculate data
-        plane_attendance = sim_shows(no_show_rates, plane_capacity + i, int(runs * 0.0001))
+        plane_attendance = sim_shows(no_show_rates, plane_capacity + i, int(runs * 0.0001), False)
         overflowed_flights, bumps, bump_rate = calc_bump(plane_attendance, plane_capacity + i)
-        total_comp, empty_seats, opportunity_lost, capacity = calc_profit(plane_attendance, bumps)
+        total_comp, empty_seats, opportunity_lost, capacity, profit = calc_profit(plane_attendance, bumps)
 
         # store data
         sim_data.loc[sim_data.index[0], sim_data.columns[i - 1]] = overflowed_flights
@@ -77,14 +89,29 @@ def overbooking_sim(data_points, runs):
         sim_data.loc[sim_data.index[4], sim_data.columns[i - 1]] = empty_seats
         sim_data.loc[sim_data.index[5], sim_data.columns[i - 1]] = opportunity_lost
         sim_data.loc[sim_data.index[6], sim_data.columns[i - 1]] = capacity
+        sim_data.loc[sim_data.index[7], sim_data.columns[i - 1]] = profit
+
+    # Graph Profit
+    if graph:
+        profit = sim_data.loc['profit']
+        profit = profit / profit.max() * 100
+        profit.index = np.arange(1, 21)
+        plt.ylim(90, 100)
+        plt.xlabel("Number of extra seats sold")
+        plt.ylabel("Profit (Percentiles of max)")
+        plt.title("Profit Per Overbooking Rate")
+        profit.plot.bar(rot=0)
+        plt.show()
 
     return sim_data
 
 
 
-#no_show_rates = gen_no_show_rates(10000)
-#plane_attendance = sim_shows(no_show_rates, 218, 1000)
+# no_show_rates = gen_no_show_rates(10000, False)
+# plane_attendance = sim_shows(no_show_rates, 218, 1000, True)
 #overflow_flights, total_bumps, bump_rate = calc_bump(plane_attendance, 218)
 # total_comp, empty_seats, opportunity_lost, capacity = calc_profit(plane_attendance, total_bumps)
 
-print(overbooking_sim(20, 10000000))
+sim_data = overbooking_sim(20, 10000000, False)
+
+print(sim_data.loc['bump rate'])
