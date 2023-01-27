5f0bb3bcdf05d0583e6d1851d38e0e87f49888ec
rayhanm17 <marrero.rayhan@gmail.com>
2023-01-27 07:47:44 -0500

    manydamnattepts

--- JN/gatorcain.py
+++ JN/gatorcain.py
@@ -1,20 +1,21 @@
-#!/usr/bin/env python3
-# Paging example using Python 3. Output in JSON.
-
-import sys
-import urllib.request
-import json
+import numpy as np
+import concurrent.futures
+import requests
 import math
+import time
 from datetime import datetime
+import json
+
+start = time.perf_counter()
+
+session = requests.session()
 
 baseUrl = "https://www.fema.gov/api/open/v1/IndividualsAndHouseholdsProgramValidRegistrations?$filter=damagedStateAbbreviation%20eq%20%27FL%27%20and%20incidentType%20eq%20%27Hurricane%27&$select=incidentType,disasterNumber,ihpAmount,floodDamageAmount,foundationDamageAmount,roofDamageAmount&$orderby=declarationDate"
 
 top = 1000  # number of records to get per call
 skip = 0  # number of records to skip
 
-webUrl = urllib.request.urlopen(baseUrl + "&$inlinecount=allpages&$select=id&$top=1")
-result = webUrl.read()
-jsonData = json.loads(result.decode())
+jsonData = session.get(baseUrl + "&$inlinecount=allpages&$select=id&$top=1").json()
 
 recCount = jsonData['metadata']['count']
 loopNum = math.ceil(recCount / top)
@@ -22,26 +23,40 @@ loopNum = math.ceil(recCount / top)
 print("START " + str(datetime.now()) + ", " + str(recCount) + " records, " + str(top) + " returned per call, " + str(
     loopNum) + " iterations needed.")
 
-outFile = open("output2.json", "a")
-outFile.write('{"IndividualsAndHouseholdsProgramValidRegistrations":[')
+insurance_data = open("insurranceClaims.json", "a")
 
-i = 0
-while i < loopNum:
-    webUrl = urllib.request.urlopen(baseUrl + "&$metadata=off&$format=jsona&$skip=" + str(skip) + "&$top=" + str(top))
-    result = webUrl.read()
+insurance_data.write('{"insuranceClaims":[')
 
-    if (i == (loopNum - 1)):
-        outFile.write(str(result[1:-1], 'utf-8') + "]}")
+def download_data(req_url):
+    response = session.get(req_url)
+
+    if response.status_code == 200:
+        print(round(int(req_url[req_url.index("skip=")+5:req_url.rindex("&")])/recCount, 4))
+        return response.json()
+
     else:
-        outFile.write(str(result[1:-1], 'utf-8') + ",")
-    i += 1
-    skip = i * top
+        print(f"Error {response.status_code}: {response.text}")
+        return
 
-    print("Iteration " + str(i) + " done")
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    urls_array = np.array([f"{baseUrl}&$metadata=off&$format=jsona&$skip={i*top}&$top={top}" for i in range(loopNum)])
 
-outFile.close()
+    results = executor.map(download_data, urls_array)
+
+    for i, f in results:
+        if i == (loopNum - 1):
+            # on the last so terminate the single JSON object
+            insurance_data.write(str(f[1:-1], 'utf-8') + "]}")
+        else:
+            insurance_data.write(str(f[1:-1], 'utf-8') + ",")
+
+insurance_data.close()
+
+finish = time.perf_counter()
 
 inFile = open("output2.json", "r")
 my_data = json.load(inFile)
-print("END " + str(datetime.now()) + ", " + str(len(my_data['femawebdisasterdeclarations'])) + " records in file")
+print("END " + str(datetime.now()) + ", " + str(len(my_data['insuranceClaims'])) + " records in file")
 inFile.close()
+
+print(f'Finished in {round(finish - start, 2)} seconds')
