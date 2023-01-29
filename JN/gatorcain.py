01e1609a5f81d42d0e117414906e1b9c61fc3159
rayhanm17 <marrero.rayhan@gmail.com>
2023-01-29 01:29:32 -0500

    download script

--- JN/gatorcain.py
+++ JN/gatorcain.py
@@ -1,62 +1,57 @@
+import urllib.request
+import json
+import time
+import math
+import threading
+from tqdm import tqdm
+from datetime import datetime
+import pandas as pd
 import numpy as np
 import concurrent.futures
-import requests
-import math
-import time
-from datetime import datetime
-import json
 
-start = time.perf_counter()
-
-session = requests.session()
-
-baseUrl = "https://www.fema.gov/api/open/v1/IndividualsAndHouseholdsProgramValidRegistrations?$filter=damagedStateAbbreviation%20eq%20%27FL%27%20and%20incidentType%20eq%20%27Hurricane%27&$select=incidentType,disasterNumber,ihpAmount,floodDamageAmount,foundationDamageAmount,roofDamageAmount&$orderby=declarationDate"
+# Base URL for this endpoint. Add filters, column selection, and sort order to this.
+baseUrl = "https://www.fema.gov/api/open/v1/IndividualsAndHouseholdsProgramValidRegistrations?$filter=damagedStateAbbreviation%20eq%20%27FL%27%20and%20incidentType%20eq%20%27Hurricane%27&$select=incidentType,disasterNumber,ihpAmount,floodDamageAmount,foundationDamageAmount,roofDamageAmount"
 
 top = 1000  # number of records to get per call
 skip = 0  # number of records to skip
 
-jsonData = session.get(baseUrl + "&$inlinecount=allpages&$select=id&$top=1").json()
+# Return 1 record with your criteria to get total record count
+webUrl = urllib.request.urlopen(baseUrl + "&$inlinecount=allpages&$select=id&$top=1")
+result = webUrl.read()
+jsonData = json.loads(result.decode())
 
+# calculate the number of calls we will need to get all of our data (using the maximum of 1000)
 recCount = jsonData['metadata']['count']
 loopNum = math.ceil(recCount / top)
 
+# ensure that only one thread can write to the file at a time
+lock = threading.Lock()
+
+start = time.perf_counter()
+
+# send some logging info to the console so we know what is happening
 print("START " + str(datetime.now()) + ", " + str(recCount) + " records, " + str(top) + " returned per call, " + str(
     loopNum) + " iterations needed.")
 
-insurance_data = open("insurranceClaims.json", "a")
 
-insurance_data.write('{"insuranceClaims":[')
+# Create empty dataframe & csv file
+df = pd.DataFrame(columns=["incidentType", "disasterNumber", "ihpAmount", "floodDamageAmount", "foundationDamageAmount", "roofDamageAmount"])
+df.to_csv('IndividualsAndHouseholdsProgramValidRegistrations.csv', index=False, mode='w')
+
+
 
 def download_data(req_url):
-    response = session.get(req_url)
-
-    if response.status_code == 200:
-        print(round(int(req_url[req_url.index("skip=")+5:req_url.rindex("&")])/recCount, 4))
-        return response.json()
-
-    else:
-        print(f"Error {response.status_code}: {response.text}")
-        return
+    # By default data is returned as a CSV, if you want to begin working with and manipulating the CSV,
+    file = urllib.request.urlopen(baseUrl + "&$format=csv" + "&$skip=" + str(skip) + "&$top=" + str(top))
+    temp_df = pd.read_csv(file, skiprows=1)
+    with lock:
+        temp_df.to_csv('IndividualsAndHouseholdsProgramValidRegistrations.csv', mode='a', index=False)
 
 with concurrent.futures.ThreadPoolExecutor() as executor:
-    urls_array = np.array([f"{baseUrl}&$metadata=off&$format=jsona&$skip={i*top}&$top={top}" for i in range(loopNum)])
+    urls_array = np.array([f"{baseUrl}&$format=csv&$skip={i * top}&$top={top}" for i in range(loopNum)])
 
-    results = executor.map(download_data, urls_array)
-
-    for i, f in results:
-        if i == (loopNum - 1):
-            # on the last so terminate the single JSON object
-            insurance_data.write(str(f[1:-1], 'utf-8') + "]}")
-        else:
-            insurance_data.write(str(f[1:-1], 'utf-8') + ",")
-
-insurance_data.close()
+    for _ in tqdm(executor.map(download_data, urls_array), total=len(urls_array)):
+        pass
 
 finish = time.perf_counter()
-
-inFile = open("output2.json", "r")
-my_data = json.load(inFile)
-print("END " + str(datetime.now()) + ", " + str(len(my_data['insuranceClaims'])) + " records in file")
-inFile.close()
-
 print(f'Finished in {round(finish - start, 2)} seconds')
